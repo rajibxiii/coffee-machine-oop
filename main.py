c694bd02a04bd222263e4c88ba6b253f6a77c2e2
muhammad_rajib <48186532+rajibxiii@users.noreply.github.com>
2024-08-24 14:29:45 +0600

    created the project coffe-machine-oop

---
+++ main.py
@@ -0,0 +1,7 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+macine_on = True
+while (macine_on):
+    prompt = input ("What would you like? (espresso/latte/cappuccino): ")
